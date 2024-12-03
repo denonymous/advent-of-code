87a4c316b1a2ed6171501006a1b49605d336d90f
Mike Johnson <mwj377@gmail.com>
2024-12-03 10:07:39 -0500

    Complete 03

--- src/2024/03/index.test.ts
+++ src/2024/03/index.test.ts
@@ -4,23 +4,23 @@ import { run1, run2 } from '.'
 test('#1 sample', () => {
   const input = fs.readFileSync(`${__dirname}/sample.txt`, 'utf-8')
   const results = run1(input)
-  expect(results).toEqual(null)
+  expect(results).toEqual(161)
 })
 
 test('#1 actual', () => {
   const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8')
   const results = run1(input)
-  expect(results).toEqual(null)
+  expect(results).toEqual(183669043)
 })
 
 test('#2 sample', () => {
   const input = fs.readFileSync(`${__dirname}/sample.txt`, 'utf-8')
   const results = run2(input)
-  expect(results).toEqual(null)
+  expect(results).toEqual(48)
 })
 
 test('#2 actual', () => {
   const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8')
   const results = run2(input)
-  expect(results).toEqual(null)
+  expect(results).toEqual(59097164)
 })
--- src/2024/03/index.ts
+++ src/2024/03/index.ts
@@ -1,5 +1,20 @@
 const debug = false
 
-export const run1 = (input: string): void => {}
+export const run1 = (input: string): number => {
+  return [ ...(input.matchAll(/mul\((\d+),(\d+)\)/g)) ]
+    .reduce((acc, curr) => acc += Number.parseInt(curr[1]) * Number.parseInt(curr[2]), 0)
+}
 
-export const run2 = (input: string): void => {}
+export const run2 = (input: string): number =>
+  [ ...(input.matchAll(/(mul\((\d+),(\d+)\)|do\(\)|don't\(\))/g)) ]
+    .reduce((acc, curr) => {
+      const enabled = curr[1] === 'do()'
+        ? true
+        : curr[1] === 'don\'t()'
+          ? false
+          : acc.enabled
+
+      return enabled && curr[0].indexOf('mul(') > -1
+        ? { enabled, sum: acc.sum + (Number.parseInt(curr[2]) * Number.parseInt(curr[3])) }
+        : { enabled, sum: acc.sum }
+    }, { enabled: true, sum: 0 }).sum
