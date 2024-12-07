13a780e0f1706bf948895dd19ead595bba824b2c
mike <mwj377@gmail.com>
2024-12-07 11:42:04 -0500

    complete 07

--- src/2024/07/index.test.ts
+++ src/2024/07/index.test.ts
@@ -4,23 +4,23 @@ import { run1, run2 } from '.'
 test('#1 sample', () => {
   const input = fs.readFileSync(`${__dirname}/sample.txt`, 'utf-8')
   const results = run1(input)
-  expect(results).toEqual(null)
+  expect(results).toEqual(3749)
 })
 
 test('#1 actual', () => {
   const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8')
   const results = run1(input)
-  expect(results).toEqual(null)
+  expect(results).toEqual(1430271835320)
 })
 
 test('#2 sample', () => {
   const input = fs.readFileSync(`${__dirname}/sample.txt`, 'utf-8')
   const results = run2(input)
-  expect(results).toEqual(null)
+  expect(results).toEqual(11387)
 })
 
 test('#2 actual', () => {
   const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8')
   const results = run2(input)
-  expect(results).toEqual(null)
+  expect(results).toEqual(456565678667482)
 })
--- src/2024/07/index.ts
+++ src/2024/07/index.ts
@@ -2,6 +2,53 @@
 
 const debug = false
 
-export const run1 = (input: string): void => {}
+export const run1 = (input: string): number => {
+  return parseInput(input)
+    .map(checkEquation)
+    .reduce((acc, curr) => acc += curr, 0)
+}
 
-export const run2 = (input: string): void => {}
+export const run2 = (input: string): number => {
+  return parseInput(input)
+    .map(checkEquation2)
+    .reduce((acc, curr) => acc += curr, 0)
+}
+
+const checkEquation = (equation: Equation): number => {
+  let totals = [ equation.operands[0] ]
+  for (let i = 1; i < equation.operands.length; i++) {
+    const newTotals = [
+      ...totals.map((v) => v + equation.operands[i]),
+      ...totals.map((v) => v * equation.operands[i])
+    ].filter((v) => v <= equation.result)
+    totals = newTotals
+  }
+
+  return totals.filter((v) => v === equation.result).length > 0 ? equation.result : 0
+}
+
+const checkEquation2 = (equation: Equation): number => {
+  let totals = [ equation.operands[0] ]
+  for (let i = 1; i < equation.operands.length; i++) {
+    const newTotals = [
+      ...totals.map((v) => v + equation.operands[i]),
+      ...totals.map((v) => v * equation.operands[i]),
+      ...totals.map((v) => Number.parseInt(`${v}${equation.operands[i]}`))
+    ].filter((v) => v <= equation.result)
+    totals = newTotals
+  }
+
+  return totals.filter((v) => v === equation.result).length > 0 ? equation.result : 0
+}
+
+const parseInput = (input: string): readonly Equation[] =>
+  input.split('\n')
+    .map((line) => {
+      const bits = line.split(':')
+      return { result: Number.parseInt(bits[0]), operands: bits[1].trim().split(/\s+/).map((s) => Number.parseInt(s)) }
+    })
+
+type Equation = {
+  result: number,
+  operands: readonly number[]
+}
